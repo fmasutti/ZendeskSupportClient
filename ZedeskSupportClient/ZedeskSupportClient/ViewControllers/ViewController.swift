5f42af83dfdc7d9a41e322a5dd9a257ba224497a
Frantiesco Masutti <fmasutti@gmail.com>
2017-10-08 20:06:01 +0100

    Added "ICSPuullToRefresh" Pod to control the "Infinite paged tickets system". Implemented paged Interface for TicketsTableView.

--- ZedeskSupportClient/ZedeskSupportClient/ViewControllers/ViewController.swift
+++ ZedeskSupportClient/ZedeskSupportClient/ViewControllers/ViewController.swift
@@ -7,26 +7,50 @@
 //
 
 import UIKit
+import ICSPullToRefresh
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var ticketsTableView: UITableView!
+    
+    let perPageTickets = 10
     var tickets: Tickets?
     var ticketsList = [TicketModel]();
     
     // MARK: View Cycle.
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Request data from ZendeskAPIManager.
-        ZendeskAPIManager.sharedInstance.getTickets(perPageTickets: nil, specificPageURL: nil, successBlock: { (tickets) in
-            self.ticketsList = tickets.tickets
+        self.ticketsTableView.addInfiniteScrollingWithHandler {
+            self.requestTicketsFromZendeskAPI()
+        }
+        self.ticketsTableView.triggerInfiniteScrolling()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    func requestTicketsFromZendeskAPI() {
+        if let lastTickets = self.tickets {
+            if (lastTickets.nextPage == nil) {
+                self.ticketsTableView.setShowsInfiniteScrolling(false)
+                return
+            }
+        }
+        
+        ZendeskAPIManager.sharedInstance.getTickets(perPageTickets: self.perPageTickets, specificPageURL: self.tickets?.nextPage, successBlock: { (tickets) in
+            self.ticketsTableView.infiniteScrollingView?.stopAnimating()
+            self.tickets = tickets
+            self.ticketsList.append(contentsOf: tickets.tickets)
             self.ticketsTableView.reloadData()
         }) { (error) in
+            self.ticketsTableView.infiniteScrollingView?.stopAnimating()
             if let error = error {
                 print(error)
             }
         }
     }
+    
 }
 
 // UITableView extension for ViewController
@@ -45,14 +69,4 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
     }
-    
-//    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat{
-//        return UITableViewAutomaticDimension
-//    }
-//
-    
 }
-
-
-
-
